f982bef168c3bf27151191e81816c8f7d6d20cdd
Rostyslav <rklyba@yahoo.com>
2021-01-21 09:08:02 +0200

    Configured 'useState' hook and 'List' component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './components/List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./components/List";
 
 function App() {
-  return <main>
-    <section className="container">
-      <h3>0 birthdays today</h3>
-      <List />
-      <button onClick={() => console.log('clicked')}>clear all</button>
-    </section>
-  </main>;
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className="container">
+        <h3> {people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => console.log("clicked")}>clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
